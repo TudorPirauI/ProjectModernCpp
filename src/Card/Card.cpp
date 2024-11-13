ee19349e12732a6aa9d3faf6a0b1482dddd7b503
mavri <me@mavri.dev>
2024-11-13 18:39:31 +0200

    feat: card implementation

---
+++ src/Card/Card.cpp
@@ -0,0 +1,15 @@
+#include "Card.h"
+
+Card::Card(const int value) : m_Value{value}, m_IsEter{false}, m_IsIllusion{false} {}
+
+void Card::SetValue(const int value) { this->m_Value = value; }
+int  Card::GetValue() const { return this->m_Value; }
+
+void Card::SetIllusion(const bool isIllusion) { this->m_IsIllusion = isIllusion; }
+bool Card::GetIsIllusion() const { return this->m_IsIllusion; }
+
+void Card::SetEter(const bool isEter) { this->m_IsEter = isEter; }
+bool Card::GetIsEter() const { return this->m_IsEter; }
+
+// std::optional<Position> Card::GetPosition() const { return this->m_Position; }
+// void                    Card::SetPosition(Position position) { this->m_Position = position; }
---
+++ src/Card/Card.h
@@ -0,0 +1,33 @@
+#define CARD_H
+
+#include <cstdint>
+#include <utility>
+
+using Position = std::pair<int, int>;
+
+class Card {
+    int m_Value;
+    // std::optional<Position> m_Position;
+    bool m_PlacedBy : 1;
+    bool m_IsIllusion : 1;
+    bool m_IsEter : 1;
+
+
+public:
+    explicit Card(int value);
+
+    void              SetValue(int value);
+    [[nodiscard]] int GetValue() const;
+
+    void               SetIllusion(bool isIllusion);
+    [[nodiscard]] bool GetIsIllusion() const;
+
+    void               SetEter(bool isEter);
+    [[nodiscard]] bool GetIsEter() const;
+
+    // [[nodiscard]] std::optional<Position> GetPosition() const;
+    // void                                  SetPosition(Position position);
+};
+
+
+#endif // CARD_H
