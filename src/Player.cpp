992e10075e18b6417da98d8d74c68d0a27a2b7a9
eduard214 <octavianpantazi@yahoo.com>
2024-11-17 16:30:49 +0200

    Remaking the player.cpp
    We restructure the entire project xD

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,132 +1,23 @@
-// Player.cpp
-
-// class Player {
-
-/*
-current game mode an uint8_t
-cardsOnHand std::vector of Card (will start with X default cards or W/E)
-
-specialCards same stuff as above, but with the Hidden and Eter card (+
-other I haven't botherd reading about)
-
-score: uint8_t -> sum of all placed cards
-
-we may move the stuff below to a different class / game class
-
-isAi: bool -> true if AI, false if human
-difficulty: enum -> easy, medium, hard, impossible
-safetySwitch: bool -> if true, if the game is lost by the human player
-their PC will BSOD / Kernel Panic
-*/
-//};
-
-#include <iostream>
-
-#include "./Game/Game.h"
 #include "Player.h"
 
-Player::Player(uint8_t gameMode, const std::vector<Card> &cards) : m_gameMode(gameMode), m_cards(cards) {}
+#include <algorithm>
+#include <utility>
 
-uint8_t Player::getGameMode() const { return m_gameMode; }
+Player::Player(std::string name, Hand hand) : m_UserName{std::move(name)}, m_Score{0}, m_Hand{std::move(hand)} {}
 
-void Player::setGameMode(uint8_t gameMode) { m_gameMode = gameMode; }
+Player::~Player() = default;
 
-uint8_t Player::getScore() const { return m_score; }
+const Hand &Player::getHand() const { return m_Hand; }
 
-void Player::setScore(uint8_t score) { m_score = score; }
+const std::string &Player::getUserName() const { return m_UserName; }
 
-void Player::setAntrenamentCards() {
-    Card card1(CardType::Normal, 1, false);
-    Card card2(CardType::Normal, 2, false);
-    Card card3(CardType::Normal, 3, false);
-    Card card4(CardType::Normal, 4, false);
+int Player::getScore() const { return m_Score; }
 
-    for (int i = 0; i < 2; ++i) {
-        m_cards.push_back(card1);
-        m_cards.push_back(card2);
-        m_cards.push_back(card3);
-    }
+// void Player::playCard(const Card &card) {
+//     const auto it = std::ranges::find(m_Hand, card);
+//     if (it != m_Hand.end()) {
+//         m_Hand.erase(it);
+//     }
+// }
 
-    m_cards.push_back(card4);
-}
-
-void Player::setDuelulVrajitorilorCards() {
-    Card card1(CardType::Normal, 1, false);
-    Card card2(CardType::Normal, 2, false);
-    Card card3(CardType::Normal, 3, false);
-    Card card4(CardType::Normal, 4, false);
-
-    for (int i = 0; i < 2; ++i) {
-        m_cards.push_back(card1);
-        m_cards.push_back(card2);
-        m_cards.push_back(card3);
-    }
-    m_cards.push_back(card2);
-    m_cards.push_back(card3);
-    m_cards.push_back(card4);
-}
-
-void Player::setDuelulElementelorCards() {
-    Card card1(CardType::Normal, 1, false);
-    Card card2(CardType::Normal, 2, false);
-    Card card3(CardType::Normal, 3, false);
-    Card card4(CardType::Normal, 4, false);
-
-    m_cards.push_back(card1);
-    for (int i = 0; i < 3; ++i) {
-        m_cards.push_back(card2);
-        m_cards.push_back(card3);
-    }
-    m_cards.push_back(card4);
-}
-
-
-std::vector<Card> Player::getCards() const { return m_cards; }
-
-void Player::showCards() const {
-    for (size_t i = 0; i < m_cards.size(); ++i) {
-        std::cout << "Card no." << i << " is ";
-        m_cards[i].showCardInfo();
-    }
-}
-
-std::string Player::getName() const {
-    return m_name;
-}
-
-void Player::calculateScore() {
-    m_score = 0;
-    for (const auto &card: m_cards) {
-        m_score += card.getValue(); // de implementat getValue in Card
-    }
-}
-
-// getCards
-
-void Player::addCard(const Card &card) {
-    m_cards.push_back(card);
-    calculateScore();
-}
-
-void Player::playCard(size_t index) {
-    if (index < m_cards.size()) {
-        m_cards.erase(m_cards.begin() + index);
-        calculateScore();
-    } else {
-        std::cout << "Invalid index!" << std::endl;
-    }
-}
-
-void Player::makeMove() {
-    if (m_isAi) {
-        switch (m_difficulty) {
-            case Difficulty::Easy:
-                break;
-            case Difficulty::Medium:
-                break;
-            case Difficulty::Hard:
-            case Difficulty::Impossible:
-                break;
-        }
-    }
-}
+void Player::giveCard(const Card &card) { m_Hand.emplace_back(card); }
