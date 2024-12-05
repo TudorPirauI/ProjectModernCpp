1e4ef5919415e202b8f94141cebf2ecf3c027658
mavri <me@mavri.dev>
2024-12-05 19:22:04 +0200

    feat: base gui menu

--- src/Interface/MainWindow.cpp
+++ src/Interface/MainWindow.cpp
@@ -2,8 +2,106 @@
 // Created by mavri on 05.12.2024.
 //
 
+#include <QFont>
+#include <QLabel>
+#include <QPushButton>
+#include <QVBoxLayout>
+
 #include "MainWindow.h"
 
-MainWindow::~MainWindow() = default;
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), m_CurrentState(GameState::MainMenu) {
+    m_StackedWidget = new QStackedWidget(this);
+    setCentralWidget(m_StackedWidget);
+    DrawMenu();
+}
 
-void MainWindow::DrawMenu() {}
+void MainWindow::DrawMenu() {
+    const auto menuWidget = new QWidget(this);
+    const auto layout     = new QVBoxLayout(menuWidget);
+
+    const auto title     = new QLabel("ETER", this);
+    QFont      titleFont = title->font();
+
+    titleFont.setPointSize(32);
+    titleFont.setBold(true);
+    title->setFont(titleFont);
+    title->setAlignment(Qt::AlignCenter);
+    layout->addWidget(title);
+
+    const auto startButton = new QPushButton("New Game", this);
+    connect(startButton, &QPushButton::clicked, this, &MainWindow::OnNewGameClicked);
+    layout->addWidget(startButton);
+
+    const auto resumeButton = new QPushButton("Resume Game", this);
+    connect(resumeButton, &QPushButton::clicked, this, &MainWindow::OnResumeGameClicked);
+    layout->addWidget(resumeButton);
+
+    const auto optionsButton = new QPushButton("Options", this);
+    connect(optionsButton, &QPushButton::clicked, this, &MainWindow::OnOptionsClicked);
+    layout->addWidget(optionsButton);
+
+    const auto exitButton = new QPushButton("Exit", this);
+    connect(exitButton, &QPushButton::clicked, this, &MainWindow::OnExitClicked);
+    layout->addWidget(exitButton);
+
+    layout->setAlignment(Qt::AlignCenter);
+
+    m_StackedWidget->addWidget(menuWidget);
+    m_StackedWidget->setCurrentWidget(menuWidget);
+}
+
+void MainWindow::DrawNewGame() {
+    const auto newGameWidget = new QWidget(this);
+    const auto layout        = new QVBoxLayout(newGameWidget);
+
+    const auto label = new QLabel("New Game Screen", this);
+    label->setAlignment(Qt::AlignCenter);
+    layout->addWidget(label);
+
+    m_StackedWidget->addWidget(newGameWidget);
+    m_StackedWidget->setCurrentWidget(newGameWidget);
+}
+
+void MainWindow::DrawResumeGame() {
+    const auto resumeGameWidget = new QWidget(this);
+    const auto layout           = new QVBoxLayout(resumeGameWidget);
+
+    const auto label = new QLabel("Resume Game Screen", this);
+    label->setAlignment(Qt::AlignCenter);
+    layout->addWidget(label);
+
+    m_StackedWidget->addWidget(resumeGameWidget);
+    m_StackedWidget->setCurrentWidget(resumeGameWidget);
+}
+
+void MainWindow::DrawOptions() {
+    const auto optionsWidget = new QWidget(this);
+    const auto layout        = new QVBoxLayout(optionsWidget);
+
+    const auto label = new QLabel("Options Screen", this);
+    label->setAlignment(Qt::AlignCenter);
+    layout->addWidget(label);
+
+    m_StackedWidget->addWidget(optionsWidget);
+    m_StackedWidget->setCurrentWidget(optionsWidget);
+}
+
+void MainWindow::OnNewGameClicked() {
+    m_CurrentState = GameState::NewGame;
+    DrawNewGame();
+}
+
+void MainWindow::OnResumeGameClicked() {
+    m_CurrentState = GameState::ResumeGame;
+    DrawResumeGame();
+}
+
+void MainWindow::OnOptionsClicked() {
+    m_CurrentState = GameState::Options;
+    DrawOptions();
+}
+
+void MainWindow::OnExitClicked() {
+    m_CurrentState = GameState::Exit;
+    close();
+}
--- src/Interface/MainWindow.h
+++ src/Interface/MainWindow.h
@@ -6,20 +6,31 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
-#include "../GameBoard/Board.h"
+#include <QStackedWidget>
+#include <memory>
 
 class MainWindow final : public QMainWindow {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow() override;
 
-    void DrawMenu();
+private slots:
+    void OnNewGameClicked();
+    void OnResumeGameClicked();
+    void OnOptionsClicked();
+    void OnExitClicked();
 
 private:
-    GameBoard *m_GameBoard;
+    enum class GameState { MainMenu, NewGame, ResumeGame, Options, Exit };
+
+    GameState       m_CurrentState;
+    QStackedWidget *m_StackedWidget;
+
+    void DrawMenu();
+    void DrawNewGame();
+    void DrawResumeGame();
+    void DrawOptions();
 };
 
 #endif // MAINWINDOW_H
