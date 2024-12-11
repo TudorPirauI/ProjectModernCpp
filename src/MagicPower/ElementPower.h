097bea981c613970f5f599a56bbea81ecdac7fae
miru28 <lupumirunacristina@gmail.com>
2024-12-11 17:32:05 +0200

    create VerifyElementalPower

--- src/MagicPower/ElementPower.h
+++ src/MagicPower/ElementPower.h
@@ -7,34 +7,34 @@
 
 #include <string>
 
+enum class ElementIndexPower {
+    ControlledExplosion,
+    Destruction,
+    Flames,
+    Lava,
+    FromAshes,
+    Sparks,
+    Blizzard,
+    Gale,
+    Hurricane,
+    Gust,
+    Mirage,
+    Storm,
+    Tide,
+    Fog,
+    Wave,
+    Whirlpool,
+    Tsunami,
+    Waterfall,
+    Support,
+    Earthquake,
+    Shattering,
+    Granite,
+    Avalanche,
+    Boulder
+};
+
 class ElementPower {
-public:
-    enum class ElementIndexPower {
-        ControlledExplosion,
-        Destruction,
-        Flames,
-        Lava,
-        FromAshes,
-        Sparks,
-        Blizzard,
-        Gale,
-        Hurricane,
-        Gust,
-        Mirage,
-        Storm,
-        Tide,
-        Fog,
-        Wave,
-        Whirlpool,
-        Tsunami,
-        Waterfall,
-        Support,
-        Earthquake,
-        Shattering,
-        Granite,
-        Avalanche,
-        Boulder
-    };
 
 private:
     ElementIndexPower RandomPower();
