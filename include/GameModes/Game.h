5aeb3e9cc49ed6913388d7475630755fa50ca138
eduard214 <143075469+eduard214@users.noreply.github.com>
2025-01-16 13:50:43 +0200

    Editor settings

--- include/GameModes/Game.h
+++ include/GameModes/Game.h
@@ -17,7 +17,7 @@
 // todo: 10 cărți: 1, 1, 2, 2, 2, 3, 3, 3, 4, Eter; fiecare jucător primește un vrăjitor aleatoriu
 // și distinct; Format meci: 3/5
 // todo: DuelulElementelor - the players must have:
-// fiecare jucător are 10 cărți: 1, 2, 2, 2, 3,  3, 3, 4, Eter; se aleg 2 puteri distincte în mod
+// fiecare jucător are 10 cărți: 1, 1, 2, 2, 2, 3,  3, 3, 4, Eter; se aleg 2 puteri distincte în mod
 // aleatoriu; cele 2 puteri pot fi folosite de amândoi jucătorii; Format meci: 3/5
 
 class Game {
