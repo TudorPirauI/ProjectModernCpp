d2daf9b07e5f66f24b596eb45b754062b583747a
eduard214 <143075469+eduard214@users.noreply.github.com>
2025-01-17 17:14:00 +0200

    Implemented the showAlert class

--- include/Interface/AlertWidget.h
+++ include/Interface/AlertWidget.h
@@ -5,6 +5,18 @@
 #ifndef ALERTWIDGET_H
 #define ALERTWIDGET_H
 
-class AlertWidget {};
+#include "pch.h"
+
+class AlertWidget : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit AlertWidget(QWidget *parent = nullptr);
+    void ShowAlert(const QString &message);
+
+private:
+    QLabel *m_MessageLabel;
+    QTimer *m_CloseTimer;
+};
 
 #endif // ALERTWIDGET_H
--- src/Interface/AlertWidget.cpp
+++ src/Interface/AlertWidget.cpp
@@ -4,8 +4,46 @@
 
 #include "../../include/Interface/AlertWidget.h"
 
-/*
- *TLDR: A box that pops up over everything else and the background is blurred
- *It had a set timer that it can be seen for (it closes automatically after that)
- *It should have a function that accepts a string and displays it
- */
+AlertWidget::AlertWidget(QWidget *parent) : QWidget(parent) {
+    setWindowFlags(Qt::FramelessWindowHint | Qt::Dialog);
+    setAttribute(Qt::WA_TranslucentBackground);
+    setAttribute(Qt::WA_ShowWithoutActivating);
+
+    auto layout    = new QVBoxLayout(this);
+    m_MessageLabel = new QLabel(this);
+    m_MessageLabel->setAlignment(Qt::AlignCenter); // Center the text within the label
+    m_MessageLabel->setStyleSheet(
+            "background-color: white; padding: 10px; border-radius: 5px; font-size: 18px; "
+            "font-weight: bold; color: #333;"); // Add styling
+
+    layout->addWidget(m_MessageLabel);
+    setLayout(layout);
+
+    m_CloseTimer = new QTimer(this);
+    connect(m_CloseTimer, &QTimer::timeout, this, &AlertWidget::close);
+}
+
+void AlertWidget::ShowAlert(const QString &message) {
+    m_MessageLabel->setText(message);
+    m_CloseTimer->start(3000);
+
+    // Apply blur effect to the parent widget
+    if (parentWidget()) {
+        auto blurEffect = new QGraphicsBlurEffect(this);
+        blurEffect->setBlurRadius(10);
+        parentWidget()->setGraphicsEffect(blurEffect);
+    }
+
+    // Start the timer and connect its timeout signal to a lambda function
+    m_CloseTimer->start(3000);
+    connect(m_CloseTimer, &QTimer::timeout, this, [this]() {
+        if (parentWidget()) {
+            parentWidget()->setGraphicsEffect(nullptr); // Remove blur effect
+        }
+        close();
+    });
+
+    show();
+    raise();
+    activateWindow();
+}
